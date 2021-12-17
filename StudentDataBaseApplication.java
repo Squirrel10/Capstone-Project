ccf8802e717e4db35255cdcd2bfaa92bb8b8acee
Patrick <67674464+Squirrel10@users.noreply.github.com>
2021-12-16 18:42:33 -0800

    my final presentation
    Extends

---
+++ StudentDataBaseApplication.java
@@ -0,0 +1,19 @@
+package studentdatabaseapplication;
+
+public class StudentDataBaseApplication {
+
+	public static void main(String[] args) {
+		
+		
+		Students stu1 = new Students();
+		stu1.enroll();
+		stu1.payTuition();
+		System.out.println(stu1.toString());
+	
+		// Ask how many new users we want to add
+		
+		//Create n number of new students	
+
+	}
+
+}
